06c6635776eb5d823e3072fc879b95ec40824a2b
Kinanee samson <kinaneesamsonjohn@gmail.com>
2022-09-09 14:45:22 +0100

    fixed some more styling

--- components/Recent-Posts.tsx
+++ components/Recent-Posts.tsx
@@ -19,14 +19,14 @@ const RecentPosts: FC<Posts> = ({ posts }) => {
   return (
     <div className="container pt-4 pb-4">
       <Row>
-        {posts && posts.map((post, index) => (
-          <Col lg={6} sm key={index} className="my-2">
-            <Container>
-              <Card.Img style={{ cursor: 'pointer'}} src={post.coverPhoto} alt="Card image" onClick={(e:any) => navigate(`/post/${post.id}`)} />
+        <Col lg={6} className="my-2">
+          {posts && posts.map((post, index) => (
+            <Container key={index}>
+              <Card.Img style={{ cursor: 'pointer' }} src={post.coverPhoto} alt="Card image" onClick={(e: any) => navigate(`/post/${post.id}`)} />
               <Card className="bg-white text-dark">
                 <Card.Body>
-                  <Card.Title style={{ cursor: 'pointer'}} onClick={(e:any) => navigate(`/post/${post.id}`)}>{post.title}</Card.Title>
-                  <Card.Text style={{ cursor: 'pointer'}} onClick={(e:any) => navigate(`/post/${post.id}`)}>
+                  <Card.Title style={{ cursor: 'pointer' }} onClick={(e: any) => navigate(`/post/${post.id}`)}>{post.title}</Card.Title>
+                  <Card.Text style={{ cursor: 'pointer' }} onClick={(e: any) => navigate(`/post/${post.id}`)}>
                     {post.description}
                   </Card.Text>
                   <Card.Text className="card-text text-muted small" onClick={(e: any) => navigate(`/profile/${post.author.id}`)}>
@@ -36,8 +36,28 @@ const RecentPosts: FC<Posts> = ({ posts }) => {
                 </Card.Body>
               </Card>
             </Container>
-          </Col>
-        ))}
+          ))}
+        </Col>
+        <Col lg={6} className="my-2">
+          {posts && posts.map((article, index) => (
+            <Container key={index}>
+              <Card className="p-4">
+                <h2 className="mb-1 h4 font-weight-bold">
+                  <a className="text-dark" onClick={(e: any) => { e.preventDefault(); navigate(`/post/${article.id}`) }}>{article.title}</a>
+                </h2>
+                <p onClick={(e: any) => navigate(`/post/${article.id}`)}>
+                  {article.description}
+                </p>
+                <div className="card-text text-muted small"
+                  onClick={(e: any) => navigate(`/profile/${article.author.id}`)}
+                >
+                  {article.author.name}
+                </div>
+                <small className="text-muted">{new Date(article.createdAt).toDateString()} · {article.readingTimeInMins} min read</small>
+              </Card>
+            </Container>
+          ))}
+        </Col>
       </Row>
     </div>
   )

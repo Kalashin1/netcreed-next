1d19eb700259a815935a403ce0c755d281a1a669
Kinanee samson <kinaneesamsonjohn@gmail.com>
2022-09-11 15:29:50 +0100

    added analytics

---
+++ pages/_app.tsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/no-unescaped-entities */
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React, { useEffect } from 'react';
+import Script from 'next/script';
+import Router from 'next/router';
+
+
+function CustomApp({ Component, pageProps, router }: AppProps) {
+
+  return (
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-8JWZ8XRQMV"></Script>
+      <Script id="h1">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          {/* @ts-ignore */}
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+
+          gtag('config', 'G-8JWZ8XRQMV');        
+        `}
+        
+      </Script>
+
+
+      <Head>
+        <title>Welcome!</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+
+export default CustomApp;

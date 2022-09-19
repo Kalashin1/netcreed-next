a6d4b36dfa9780ce78a5e54d0f992607d56da2f2
Kinanee samson <kinaneesamsonjohn@gmail.com>
2022-09-19 08:14:12 +0100

    added some feature

--- components/Post-Header.tsx
+++ components/Post-Header.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { Article } from '../types';
 import Image from 'next/image';
 import { useRouter } from 'next/router'
+const marked = require('marked');
 
 type _Article = {
   article: Article
@@ -41,8 +42,8 @@ const PostHeader: FC<_Article> = ({ article }) => {
                 >Edit</a>
               </p>)}
               <h1 className="display-4 secondfont mb-3 font-weight-bold">{article.title}</h1>
-              <p className="mb-3">
-                {article.description}...
+              <p className="mb-3" dangerouslySetInnerHTML={{ __html: marked.marked(article.description) }}>
+        
               </p>
               <div className="d-flex align-items-center">
                 { /* eslint-disable-next-line @next/next/no-img-element */}

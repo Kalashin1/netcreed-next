c4a4289c3e6a40313c4766dd7f5c47910cc417c6
Kinanee samson <kinaneesamsonjohn@gmail.com>
2022-09-19 11:09:45 +0100

    removed twitter card tags

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -56,10 +56,10 @@ const Post: NextPage = ({ article, articles }) => {
         <meta name="revisit-after" content="3 days" />
         <meta name="author" content={article.author.name} />
          {/* TWITTER CARD  */}
-        <meta name="twitter:card" content="summary_large_image" />
+        {/* <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={article.title} />
         <meta name="twitter:description" content={article.description} />
-        <meta name="twitter:image" content={article.coverPhoto} />
+        <meta name="twitter:image" content={article.coverPhoto} /> */}
         {/* Open Graph  */}
         <meta property="og:type" content="article" />
         <meta property="og:url" content={`https://netcreed-blog.vercel.app/post/${article.id}`} />

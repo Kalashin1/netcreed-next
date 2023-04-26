d8011debe7540c2ba9494bbbda3c3472de7ceb8d
Kinanee Samson <kinaneesamsonjohn@gmail.com>
2023-04-26 11:21:13 +0100

    fixed slight bug

--- components/Profile-view.tsx
+++ components/Profile-view.tsx
@@ -20,7 +20,7 @@ import {
   getAllUserArticles,
   getProfile,
 } from '../helper';
-import { Plus } from './svg/plus';
+import Plus from './svg/plus';
 const marked = require('marked');
 
 type UserPropsType = {
